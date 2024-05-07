4e06f80123ef40567e8b56401972995f8b542feb
Pratiksha Gunjal <gunjalpratiksha2003@gmail.com>
2024-05-07 18:30:32 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Restaurant from './Components/Basics/Restaurant'
+
+const App = () => {
+  return <Restaurant/>
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'   //After version 17, no need of writing this
+
+// const App = () => {
+//   return (
+//     <>
+//       <Compo />
+//       <div>Welcomeee</div>
+//     </>
+//     // <></>      ...this means react Fragment.
+//   );
+// }
+
+// const Compo =()=>{
+//   return(
+//     <div>Hello!</div>
+//   )
+// }
+
+// export default App
